a5948d4e30fb76371f88e30c28541d6cccd1dd4b
Carles Jove <info@joanielena.cat>
2012-09-28 13:40:56 +0200

    Only show categories if they exist

--- system/views/cms/posts/add.php
+++ system/views/cms/posts/add.php
@@ -36,13 +36,11 @@
 			<?php endforeach; ?>
 		</select>
 		
-		<label for="categories"><?php echo $this->cms->message('categories'); ?></label>
 		<?php if($this->have($cats)): ?>
+		<label for="categories"><?php echo $this->cms->message('categories'); ?></label>
 			<?php foreach($cats as $cat): ?>
 				<input type="checkbox" name="categories[]" value="<?php echo $cat->identifier; ?>" /> <?php echo $cat->title; ?>
 			<?php endforeach; ?>
-		<?php else: ?>
-			<input type="text" name="categories[]" value="<?php echo $this->cms->input_for('categories', 'Uncategorized'); ?>" />
 		<?php endif; ?>
 				
 		<label for="author"><?php echo $this->cms->message('author'); ?></label>
