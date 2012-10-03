2298f6ba5fe93db40184d38c8a60c23a2294be76
Carles Jove <info@joanielena.cat>
2012-10-03 11:04:16 +0200

    Validates presence of description

--- system/models/post.php
+++ system/models/post.php
@@ -10,6 +10,7 @@ class Post extends ActiveRecord\Model
 	static $validates_presence_of = array(
 		array('title', 'message' => 'This field cannot be empty'),
 		array('content', 'message' => 'This field cannot be empty'),
+		array('description', 'message' => 'This field cannot be empty'),
 		array('author', 'message' => 'Posts must have an author'),
 		array('section', 'message' => 'Posts must belong to a site section')
 	);
