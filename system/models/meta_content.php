4e899628cdaac0452f051763201e164d87fed9d5
Carles Jove <info@joanielena.cat>
2012-10-03 11:43:04 +0200

    Added what should be the new combination

--- system/models/meta_content.php
+++ system/models/meta_content.php
@@ -8,7 +8,7 @@
 class Meta_content extends ActiveRecord\Model
 {
 	static $validates_uniqueness_of = array(
-		array('nice_url', 'message' => 'title is repeated!'),
-//		array(array('nice_url','in_table'), 'message' => 'blah and bleh!')
+//		array('nice_url', 'message' => 'title is repeated!'),
+		array(array('nice_url','in_table','parent'), 'message' => 'There is already one content with this URL')
 	);
 }
