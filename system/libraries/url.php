a51491aaf3059e89300af961dc0b4da9047a2ae2
Carles Jove i Buxeda <info@joanielena.cat>
2012-10-11 10:36:02 +0200

    Generated class code. Returns full path awesomely ;-)

--- system/libraries/url.php
+++ system/libraries/url.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * URL class
- * Gets off you the stupid job of writing URLs 
+ * Gets off you the boring job of writing URLs 
  * 
  * @package Safanoria
  * @subpackage Libraries
@@ -10,13 +10,59 @@
  * 		 
  */
 
-class Url extends Safanoria
+class Url
 {
+	public $full_path;
+	public $segments = array();	
+
 	/**
-	 *
+	 * Returns the full path to a database object
+	 * @param int
+	 * @param bool
+	 * @return string
 	 */
-	public function full_path_to()
+	public function full_path_to($id, $absolute = TRUE)
 	{
-		return $_SESSION['lang'];
+		$meta = Meta_content::find($id);
+
+		$this->segments[] = $meta->nice_url;
+		$this->segments = self::get_parent_urls($meta->parent);
+		array_unshift($this->segments, $_SESSION['lang']);
+		
+		$this->full_path = implode('/', $this->segments);
+
+		if ($absolute === TRUE) 
+		{
+			$this->full_path = '/'.$this->full_path;
+		}
+
+		return $this->full_path;
+	}
+
+	/**
+	 * Recursively looks for a parent object in the database
+	 *
+	 * @param int
+	 * @return array
+	 */
+	private function get_parent_urls($parent)
+	{
+		if ($parent > 0) 
+		{
+			$meta = Meta_content::find($parent);
+			array_unshift($this->segments, $meta->nice_url);
+			return self::get_parent_urls($meta->parent);
+		}
+		return $this->segments;
+	}
+
+	/**
+	 * Returns full path to database object in array
+	 *
+	 * @return array
+	 */
+	public function get_segments()
+	{
+		return $this->segments;
 	}
 }
