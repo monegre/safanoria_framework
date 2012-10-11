6f4acd5e75f80f17961b60a5a6ab3d47bb3ce684
Carles Jove i Buxeda <info@joanielena.cat>
2012-10-11 02:01:13 +0200

    Added config array. Moved non configurable code to settings.php. Checks for existance of settings.php

--- install/files/config.model.php
+++ install/files/config.model.php
@@ -9,22 +9,10 @@
  * Loads settings.php
  */
 
-/** 
- * Display errors. FALSE by default
- * Only set to TRUE on developement 
- */
-$display_errors = FALSE;
-if ($display_errors === TRUE) {
-	ini_set('display_errors', 'On');
-	error_reporting(E_ALL | E_STRICT);
-//	error_reporting( E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING | E_RECOVERABLE_ERROR | E_STRICT);
-} else {
-	ini_set('display_errors', 'Off');
-}
+$config['underscore_routes'] = TRUE;
+$config['display_errors'] = FALSE;
 
-/**
- * Database settings  
- */
+/** Database settings */
 define ('DB_NAME', '[db_name]'); // Database name
 define ('DB_USER', '[db_user]'); // Database user
 define ('DB_PASS', '[db_pass]'); // Database pass
@@ -33,12 +21,15 @@ define ('DB_CHARSET', 'utf8'); // Character encoding
 define ('DB_DSN', 'mysql:host=[db_host];dbname=[db_name]'); // If you're ever using a rough PDO connection
 define ('DB_TABLE_PREFIX', '[db_table_prefix]'); // Tables prefix
  
-/**
- * Edition ends here. Go have a tea.
- */
-/** Define the root of Safanòria's installation */
- if ( !defined('ROOT') )
- 	define('ROOT', dirname(dirname(__FILE__)) . '/');
 
+/** Configurations ends here. Go have a tea */
 /** Sets common variables and paths to directories **/
-require_once (ROOT . 'config/settings.php');
+$filename = 'settings.php';
+if(file_exists($filename))
+{
+	require_once($filename);
+}
+else
+{
+	exit('We could not find the '.$filename.' file. This is a fatal error.');
+}
