17b73f602ad8569631acbe90b900736445816b70
Carles Jove i Buxeda <info@joanielena.cat>
2012-10-11 02:03:10 +0200

    Calls Router class

--- public/index.php
+++ public/index.php
@@ -6,8 +6,16 @@
  * This file also calls de Routing() function, which calls the correspondent controller
  */
 
-require_once ( dirname(dirname(__FILE__)) . '/config/config.php' );
+if (file_exists(dirname(dirname(__FILE__)) . '/config/config.php')) 
+{
+	require_once ( dirname(dirname(__FILE__)) . '/config/config.php' );
+}
+else 
+{
+	exit('We could not find the config file. Safanoria needs this file in order to run. This is a fatal error.');
+}
+
 
 /** Get the $url and send it to the router */
 $url = isset($_GET['url']) ? $_GET['url'] : "home";
-Routing($url);
+new Router($url);
