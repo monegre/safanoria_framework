37977b9c05b9d60d9b0624f5da7a95b26a8b3032
Carles Jove i Buxeda <info@joanielena.cat>
2012-10-12 19:33:23 +0200

    Mv hardcoded text to variables

--- system/views/rss.php
+++ system/views/rss.php
@@ -1,31 +1,31 @@
-
 <?php header("Content-type: text/xml"); ?>
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+	<?php echo '<rss version="2.0">'; ?>
 
-<?xml version="1.0" encoding="UTF-8"?>
-
-<?php 
-//Cabeceras del RSS
-//echo '<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">'; ?>
-
-<rss xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:sy="http://purl.org/rss/1.0/modules/syndication/" xmlns:slash="http://purl.org/rss/1.0/modules/slash/" version="2.0">
-
+<?php echo 
+('
 <channel>
-	<title>Lo títol del lloc web</title>
-	<link>http://exemple.com</link>
-	<description>Hola, sóc la descripció del lloc web</description>
-	<language>ca</language>
-	<copyright>Copyright?</copyright>
+	<title>'.$this->current['page_title'].'</title>
+	<link>'.$this->current['site_url'].'</link>
+	<description>'.$this->current['site_description'].'</description>
+	<language>'.$this->current['feed_lang'].'</language>
+	<copyright>'.$this->current['copyright'].'</copyright> 
+');
 
-<?php foreach($list as $item): ?>
-
+foreach($list as $item):
+	echo 
+	('
 	<item>
-		<title><?php echo $item->title; ?></title>
-		<description><?php echo $item->content; ?></description>
-		<link><?php echo $this->url->full_path_to($item->identifier); ?></link>
+		<title>'.$item->title.'</title>
+		<description>'.$item->content.'</description>
+		<link>'.$this->url->full_path_to($item->identifier).'</link>
 		<pubDate></pubDate>
 	</item>
+	');
+endforeach;
 
-<?php endforeach; ?>
-
+echo 
+('
 	</channel>
 </rss>
+'); 
