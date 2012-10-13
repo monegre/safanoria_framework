656f3003bb3fcdd8409deab691cd3872d10dbc24
Carles Jove i Buxeda <info@joanielena.cat>
2012-10-14 01:34:42 +0200

    Added routes. Now you can remap requests. YEAH

---
+++ config/routes.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Routes
+ *
+ * Here you can re-map URL requests to specific controller functions.
+ * Typically there is a one-to-one relationship between a URL string
+ * and its corresponding controller class/method. The segments in a
+ * URL normally follow this pattern:
+ *
+ *  example.com/class/method/id/
+ *
+ * In some instances, however, you may want to remap this relationship
+ * so that a different class/function is called than the one
+ * corresponding to the URL.
+ *
+ *  Example:
+ *  $route['canela/en-rama'] = 'class/my_method';
+ */
+
+$route['default_controller'] = 'home';
+$route['default_method'] = 'index';
