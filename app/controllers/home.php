8b327770d79ef690f5ef24e516b89f4a1678c3bc
Carles Jove i Buxeda <info@joanielena.cat>
2012-10-18 12:01:34 +0200

    Has customziable error methods

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -1,6 +1,10 @@
 <?php
 /**
+ * Home controller
  *
+ * This is the default controller that comes with Safanòria installation.
+ * As such, it is the default_controller in config/routes
+ * It comes with customizable show_404() and error() methods.
  */
  
 class Home extends SF_Controller
@@ -21,12 +25,26 @@ class Home extends SF_Controller
 	{
 		require $this->view('benvinguda');
 	}
-		
+	
 	/**
-	 * 
+	 * Here you can custom your 404 error page.
+	 * You can also create an app_errors controller
+	 * or map error through the config/routes
 	 */
-	function error() 
+	public function show_404() 
 	{
 		require $this->view('404');
+		exit;
+	}
+		
+	/**
+	 * Here you can custom your generic error page
+	 * You can also create an app_errors controller
+	 * or map error through the config/routes
+	 */
+	public function error() 
+	{
+		require $this->view('error');
+		exit;
 	}	
 }
