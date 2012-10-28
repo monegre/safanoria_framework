8e8a985d3ba2c3d5cf8456d2372ad3ccfd767d56
Carles Jove i Buxeda <info@joanielena.cat>
2012-10-28 08:51:00 +0100

    styled with bootstrap

--- system/views/cms/index.php
+++ system/views/cms/index.php
@@ -1,10 +1,10 @@
 <div class="hero-unit">
-  <h1>Benviguts a Safanòria!</h1>
-  <p>El primer pas per gestionar el teu web és crear les seccions de que es composarà.</p>
-  <p>
-    <a class="btn">
-      Som-hi!
-    </a>
-  </p>
+	<h1>Benvinguts a Safanòria!</h1>
+	<p>El primer pas per gestionar el teu web és crear les seccions de que es composarà.</p>
+	<p>Vols crear la primera?</p>
+	<p>
+		<a href="<?php echo $this->current['new_item']; ?>" class="btn btn-primary btn-large">
+		Som-hi!
+		</a>
+	</p>
 </div>
-
