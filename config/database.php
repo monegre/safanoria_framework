5c16c40173f24baa778c8cbfdbeaa74e8d7902dc
Carles Jove i Buxeda <info@joanielena.cat>
2012-11-05 23:21:31 +0100

    Test database

--- config/database.php
+++ config/database.php
@@ -9,10 +9,10 @@
  */
 
 /** Database settings */
-define ('DB_NAME', '[database]'); // Database name
-define ('DB_USER', '[user]'); // Database user
-define ('DB_PASS', '[pass]'); // Database pass
+define ('DB_NAME', 'sf_real_test'); // Database name
+define ('DB_USER', 'root'); // Database user
+define ('DB_PASS', 'root'); // Database pass
 define ('DB_HOST', 'localhost'); // Database host
 define ('DB_CHARSET', 'utf8'); // Character encoding
-define ('DB_DSN', 'mysql:host=localhost;dbname=[database]'); // If you're ever using a rough PDO connection
+define ('DB_DSN', 'mysql:host=localhost;dbname=sf_real_test'); // If you're ever using a rough PDO connection
 define ('DB_TABLE_PREFIX', '[db_table_prefix]'); // Tables prefix
